a1540a1ad8ba1de473ccbcfd2d37fc2d3f7baa48
Pinutik <jan.bela007@gmail.com>
2023-10-20 08:14:48 +0200

    mala nasobilka

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ahoj ja som virtualny papusek Lora, rad oapkujem");
+Console.WriteLine("napis nieco co budem opakovat");
+
+string vstup;
+vstup = Console.ReadLine();
+
+string vystup;
+vystup= vstup + "," + vstup + "!";
+
+Console.WriteLine(vystup);
+Console.ReadKey();
+
---
+++ jano belo/jano belo/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vitajte v kalkulacke");
+Console.WriteLine(" zadajte prve cislo");
+float a = float.Parse(Console.ReadLine();
+Console.WriteLine("zadajtre druhe cislo":);
+float b = float.Parse(Console.ReadLine());
+float sucet = a + b;
+float rozdiel = a - b;
+float sucin = a * b;
