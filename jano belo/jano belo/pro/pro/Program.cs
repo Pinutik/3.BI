fd9f95686b868790f40899a6951e674efc2d410d
Pinutik <jan.bela007@gmail.com>
2023-10-20 08:43:07 +0200

    nasobilka

--- jano belo/jano belo/pro/pro/Program.cs
+++ jano belo/jano belo/pro/pro/Program.cs
@@ -4,16 +4,16 @@ class Program
 {
     static void Main()
     {
-        int horniMez = 10; 
+        int horniMez = 10;
 
         for (int i = 1; i <= horniMez; i++)
         {
             for (int j = 1; j <= horniMez; j++)
             {
                 int vysledek = i * j;
-                Console.Write($"{vysledek}\t"); 
+                Console.Write($"{vysledek}\t");
             }
-            Console.WriteLine(); 
+            Console.WriteLine();
         }
     }
 }
